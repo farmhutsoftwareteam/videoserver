812f923233401f9b38547b3ae51b8c85f773547a
Munyaradzi Makosa <munya@farmhutafrica.com>
2024-03-25 13:54:00 +0200

    pipelinzes

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.get('/', (req, res) => {
-    res.send('Video server is up and running!');
+    res.send('Video server is up and running on pipelinez!');
 });
 
 app.listen(PORT, () => {
