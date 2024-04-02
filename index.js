c8cfafbc25e307536a186385d8ef1877bab9f697
Munyaradzi Makosa <munya@farmhutafrica.com>
2024-04-02 12:20:58 +0200

    port 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const videoRoutes = require('./routes/video');
 const userRoutes = require('./routes/user'); // Adjust the path to your user routes file
 const swaggerUi = require('swagger-ui-express');
