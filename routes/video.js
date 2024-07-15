d9fbf4860be4dbf0c55337b4578309f3e9f1b4a8
Ryan Katayi <108867595+RyanKatayi@users.noreply.github.com>
2024-07-15 10:30:23 +0200

    feat: changes

--- routes/video.js
+++ routes/video.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
 const blobServiceClient = new BlobServiceClient(
-  'https://heartandsoulstorage.blob.core.windows.net?sv=2022-11-02&ss=bfqt&srt=o&sp=rwdlacupiytfx&se=2025-12-07T17:17:30Z&st=2024-07-12T09:17:30Z&spr=https&sig=mj188XaUBx9L3Qfw6xOpaSfBhdDrbygW%2F3ZU5P41Xbk%3D'
+  'https://hstv.blob.core.windows.net?sv=2022-11-02&ss=bfqt&srt=o&sp=rwdlacupiytfx&se=2025-12-07T17:17:30Z&st=2024-07-12T09:17:30Z&spr=https&sig=mj188XaUBx9L3Qfw6xOpaSfBhdDrbygW%2F3ZU5P41Xbk%3D'
 );
 const containerClient1 = blobServiceClient.getContainerClient('thumbnails');
 const containerClient2 = blobServiceClient.getContainerClient('videos');
